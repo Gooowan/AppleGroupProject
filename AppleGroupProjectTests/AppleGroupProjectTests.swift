9559c3160fba1bc451bbd57d75fba7cd6e82ac2a
BUNJEE <vsolomatin@kse.org.ua>
2024-12-10 19:45:57 +0200

    Initial Commit

---
+++ AppleGroupProjectTests/AppleGroupProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleGroupProjectTests.swift
+//  AppleGroupProjectTests
+//
+//  Created by Ivan Solomatin on 10.12.2024.
+//
+
+import Testing
+@testable import AppleGroupProject
+
+struct AppleGroupProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
