21a7fa06440d5bf8f62d4792401364e4fad3c8d0
BUNJEE <vsolomatin@kse.org.ua>
2024-12-17 23:39:12 +0200

    added EntitiesManager

---
+++ AppleGroupProject/Model/EntitiesManager.swift
@@ -0,0 +1,72 @@
+//
+//  EntitiesManager.swift
+//  AppleGroupProject
+//
+//  Created by Ivan Solomatin on 17.12.2024.
+//
+
+import Foundation
+
+
+final class EntitiesManager {
+    static let shared = EntitiesManager()
+    
+    var users: [User] = []
+    var quotes: [Quote] = []
+    
+    private init() {
+        users = loadUsers()
+        quotes = loadQuotes()
+    }
+    
+    func loadUsers() -> [User] {
+        // load all users from backend
+        return []
+    }
+    
+    func loadQuotes() -> [Quote] {
+        // load all quotes from backend
+        return []
+    }
+    
+    func addUser(user: User) {
+        users.append(user)
+        // add requesting for backend
+    }
+    
+    func removeUser(user: User) {
+        users.removeAll { $0.id == user.id }
+        // add requesting for backend
+    }
+    
+    func addQuote(quote: Quote) {
+        quotes.append(quote)
+        // add requesting for backend
+    }
+    
+    func removeQuote(quote: Quote) {
+        quotes.removeAll { $0.text == quote.text }
+        // add requesting for backend
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+Message Andrii Klykavka, David Bakalov ITBA26, Kseniia Hanziuk
+
+
+
+
+
+
+
+
+
